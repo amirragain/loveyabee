bbe7da659bb31c9d4f38de51ca145a5cfca9bd97
amirragain <wagasal@students.nu-laguna.edu.ph>
2025-06-09 21:57:52 +0800

    Add files via upload

---
+++ letter.js
@@ -0,0 +1,43 @@
+let heart = document.querySelector('.heart')
+let card = document.querySelector('.card')
+let box = document.querySelector('#box')
+heart.addEventListener('click',function(){
+    card.setAttribute("style","opacity:0");
+    let x = document.createElement("audio");
+     x.setAttribute("src", "qlx.mp3");
+     x.setAttribute("autoplay","autoplay");
+
+// 打字效果
+let i =0
+let str = 'Ever since I saw you my heart felt like it was about to explode and my mind just went blah blah blah blah blah I love her now.            Im really love you bee'
+let strp = ''
+
+function print()
+{
+ if(str[i]=='<')
+ {
+     document.getElementById("box").innerHTML=strp+"<br><br>+'|'";
+     strp+="<br><br>";
+ }
+ else
+ {
+     strp+=str[i];
+     box.innerHTML=strp + '|';
+ }
+ i++;
+}
+setTimeout(() => {
+    let printid=setInterval(() => {
+        print();
+        if(i==str.length)
+        clearInterval(printid);
+        },190);  
+}, 5500);  
+function appearBackground(){
+setTimeout(()=>{
+box.style.opacity=1
+},1500)
+}
+appearBackground()
+
+})
